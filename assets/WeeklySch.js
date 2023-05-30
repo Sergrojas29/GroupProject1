e960d9762331318bab421fefa370965730f7fab4
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-05-30 18:46:40 -0400

    initilize local storage

--- assets/WeeklySch.js
+++ assets/WeeklySch.js
@@ -1,42 +1,51 @@
+function initilizeLocalStorage() {
+  var dataSaved = JSON.parse(localStorage.getItem("saved"));
+
+  if (!dataSaved) {
+    localStorage.setItem("saved", JSON.stringify([]));
+  }
+}
+initilizeLocalStorage();
+
 function saveCalories() {
-    var dayOfTheWeek = dayjs().format('dddd')
-    localStorage.setItem('saved', JSON.stringify(weeklyMeal))
+  // var dayOfTheWeek = dayjs().format('dddd')
+  localStorage.setItem("saved", JSON.stringify(weeklyMeal));
 }
 
 var weeklyMeal = {
-    monday: {
-        breakfast: breakfastCalories,
-        lunch: lunchCalories,
-        dinner: dinnerCalories,
-    },
-    tuesday: {
-        breakfast: breakfastCalories,
-        lunch: lunchCalories,
-        dinner: dinnerCalories,
-    },
-    wednesday: {
-        breakfast: breakfastCalories,
-        lunch: lunchCalories,
-        dinner: dinnerCalories,
-    },
-    thursday: {
-        breakfast: breakfastCalories,
-        lunch: lunchCalories,
-        dinner: dinnerCalories,
-    },
-    friday: {
-        breakfast: breakfastCalories,
-        lunch: lunchCalories,
-        dinner: dinnerCalories,
-    },
-    saturday: {
-        breakfast: breakfastCalories,
-        lunch: lunchCalories,
-        dinner: dinnerCalories,
-    },
-    sunday: {
-        breakfast: breakfastCalories,
-        lunch: lunchCalories,
-        dinner: dinnerCalories,
-    }
-}
+  monday: {
+    breakfast: breakfastCalories,
+    lunch: lunchCalories,
+    dinner: dinnerCalories,
+  },
+  tuesday: {
+    breakfast: breakfastCalories,
+    lunch: lunchCalories,
+    dinner: dinnerCalories,
+  },
+  wednesday: {
+    breakfast: breakfastCalories,
+    lunch: lunchCalories,
+    dinner: dinnerCalories,
+  },
+  thursday: {
+    breakfast: breakfastCalories,
+    lunch: lunchCalories,
+    dinner: dinnerCalories,
+  },
+  friday: {
+    breakfast: breakfastCalories,
+    lunch: lunchCalories,
+    dinner: dinnerCalories,
+  },
+  saturday: {
+    breakfast: breakfastCalories,
+    lunch: lunchCalories,
+    dinner: dinnerCalories,
+  },
+  sunday: {
+    breakfast: breakfastCalories,
+    lunch: lunchCalories,
+    dinner: dinnerCalories,
+  },
+};
